089dea4785ce734b8a0b7b3d3d130079b011606b
abdikeem <abdihakimabdirahman21@gmail.com>
2022-07-26 01:03:21 +0300

    contracts/

--- contracts/dai.sol
+++ contracts/dai.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.15;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Dai is ERC20 {
-    constructor() EERC20('Dai Stablecoin', 'DAI') public {}
+    constructor() ERC20('Dai Stablecoin', 'DAI') public {}
         function faucet(address to, uint amount) external {
             _mint(to, amount);
     }
